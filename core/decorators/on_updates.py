d00f479ba33b0135dd782d4a1e3e2f4a8ba01d44
ByteCntrl <bytecntrl@gmail.com>
2023-08-06 00:01:01 +0200

    Add filters, update on_update, start command

--- core/decorators/on_updates.py
+++ core/decorators/on_updates.py
@@ -3,13 +3,18 @@ import typing
 
 from aiogram.types.base import TelegramObject
 
+from core.utilities import filters
 
-def on_update(type_handler: str):
+
+def on_update(type_handler: str, filters: filters = filters.all):
     def decorator(func: typing.Callable):
         func.type_handler = type_handler
 
         @functools.wraps(func)
         async def wrapper(update: TelegramObject):
+            if not (await filters(update)):
+                return
+
             return await func(update)
 
         return wrapper
--- core/plugins/start.py
+++ core/plugins/start.py
@@ -1,8 +1,9 @@
 from aiogram.types import Message
 
 from core.decorators import on_update
+from core.utilities import filters
 
 
-@on_update("message")
+@on_update("message", filters.command(["start"]))
 async def start(message: Message):
-    await message.reply("AA")
+    await message.reply("Hi!")
